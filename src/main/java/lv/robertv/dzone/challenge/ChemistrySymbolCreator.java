a6229a5322c0629f1143c34d1be4462f7e658b59
roberts_v <Roberts.Vartins@Knowledgeprice.com>
2016-08-09 21:36:17 +0300

    Tests and dummy impl for first bonus challenge

---
+++ src/main/java/lv/robertv/dzone/challenge/ChemistrySymbolCreator.java
@@ -0,0 +1,17 @@
+package lv.robertv.dzone.challenge;
+
+/**
+ * This is a chemistry symbol creator to get a first (alphabetical order) valid symbol.
+ * Dzone challenge: https://dzone.com/articles/java-code-challenge-chemical-symbol-naming-part-on
+ * @author Roberts Vartins
+ */
+public class ChemistrySymbolCreator {
+	
+	public String genFirstSymbol(String elementName) {
+		
+		String symbol = "";
+		
+		return symbol;
+	}
+	
+}
---
+++ src/test/java/lv/robertv/dzone/challenge/TestChemistrySymbolCreator.java
@@ -0,0 +1,32 @@
+package lv.robertv.dzone.challenge;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This is a test class for a chemistry symbol creator. 
+ * Dzone challenge: https://dzone.com/articles/java-code-challenge-chemical-symbol-naming-part-on
+ * @author Roberts Vartins
+ */
+public class TestChemistrySymbolCreator {
+	
+	private ChemistrySymbolCreator creator;
+	
+	@Before
+	public void setUp()
+	{
+		creator = new ChemistrySymbolCreator();
+	}
+	
+	@Test
+	public void genFirstSymbol1() {
+		Assert.assertEquals("Ee", creator.genFirstSymbol("Gozerium"));
+	}
+	
+	@Test
+	public void genFirstSymbol2() {
+		Assert.assertEquals("Ie", creator.genFirstSymbol("Slimyrine"));
+	}
+	
+}
