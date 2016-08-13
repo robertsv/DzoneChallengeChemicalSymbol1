0942f0872ea54805188f996da92116352f1fb1c5
roberts_v <Roberts.Vartins@Knowledgeprice.com>
2016-08-13 12:45:53 +0300

    Second bonus challange - tests

---
+++ src/main/java/lv/robertv/dzone/challenge/ChemistrySymbolCounter.java
@@ -0,0 +1,16 @@
+package lv.robertv.dzone.challenge;
+
+/**
+ * This is a chemistry symbol counter to number of distinct valid symbols. Dzone challenge:
+ * https://dzone.com/articles/java-code-challenge-chemical-symbol-naming-part-on
+ * 
+ * @author Roberts Vartins
+ */
+public class ChemistrySymbolCounter {
+
+	public int getDistinctSymbolCnt(String elementName) {
+		
+		return 0;
+	}
+
+}
---
+++ src/test/java/lv/robertv/dzone/challenge/TestTestChemistrySymbolCreator.java
@@ -0,0 +1,27 @@
+package lv.robertv.dzone.challenge;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * This is a test class for a chemistry symbol counter. Dzone challenge:
+ * https://dzone.com/articles/java-code-challenge-chemical-symbol-naming-part-on
+ * 
+ * @author Roberts Vartins
+ */
+public class TestTestChemistrySymbolCreator {
+
+	private ChemistrySymbolCounter counter;
+
+	@Before
+	public void setUp() {
+		counter = new ChemistrySymbolCounter();
+	}
+
+	@Test
+	public void getDistinctSymbolCnt1() {
+		Assert.assertEquals(11, counter.getDistinctSymbolCnt("Zuulon "));
+	}
+
+}
